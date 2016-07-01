967c606cb6e47cd9a3cfa2197d0583e9dd19b044
Arpit Aggarwal <aggarwalarpit.89@gmail.com>
2016-07-01 17:04:17 +0400

    first commit

---
+++ app/src/main/java/com/example/arpitaggarwal/hello_world/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.arpitaggarwal.hello_world;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ArpitAggarwal on 29/06/16.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
